168f56db584915d89ded1b36b8bb57d780eb0931
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-21 12:09:17 +0100

    Aggiungere i file di progetto.

---
+++ ClearAndConcise/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// Modificare l'applicazione precedente in modo da utilizzare la sintassi del metodo di LINQ.
+
+Random random = new Random();
+
+List<int> listaInteri = Enumerable.Range(1, 1000).Select(numeroCauale => random.Next(numeroCauale)).Where(numero => numero % 2 != 0).ToList();
+
+listaInteri.ForEach(numero => Console.WriteLine(numero));
